6172b176d359d62f8c0c043ca963e8754d832de8
mossnana <samurai.hanso@gmail.com>
2019-05-21 23:08:28 +0700

    Count Activity : Point and Reset Event

--- app/src/main/java/com/mossnana/loginactivity/CountActivity.kt
+++ app/src/main/java/com/mossnana/loginactivity/CountActivity.kt
@@ -3,12 +3,31 @@ package com.mossnana.loginactivity
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_count.*
 
 class CountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_count)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        pointLeft.setOnClickListener {
+            var currentPointLeft = pointLeft.text.toString().toInt()
+            pointLeft.setText((currentPointLeft+1).toString())
+        }
+
+        pointRight.setOnClickListener {
+            var currentPointRight = pointRight.text.toString().toInt()
+            pointRight.setText((currentPointRight+1).toString())
+        }
+
+        btnReset.setOnClickListener {
+            val resetScore: Int = 0
+            pointLeft.setText((resetScore).toString())
+            pointRight.setText((resetScore).toString())
+        }
     }
 }
--- app/src/main/java/com/mossnana/loginactivity/MainActivity.kt
+++ app/src/main/java/com/mossnana/loginactivity/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mossnana.loginactivity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAddActivity.setOnClickListener {
+            intent = Intent(this, CountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
